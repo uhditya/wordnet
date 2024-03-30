1a78e83bfd8011ac4a1ef9e74cc70f1c61a88f46
uhditya <aditya.grg121@gmail.com>
2024-03-30 11:58:14 +0530

    Added backend data fetching

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,32 +1,44 @@
 import React from "react";
-import "../CSS/SearchBar.css"
-import magnifyingGlass from "../Assests/Images/mag-glass.png"
+import "../CSS/SearchBar.css";
+import magnifyingGlass from "../Assests/Images/mag-glass.png";
 import { useState } from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState(" ");
 
-    const [query, setQuery] = useState("");
-
-    console.log(query)
-
-    return(
-        <div className = "search-bar">
-            <div className = "search-button">
-                <button className="drop-btn">Lemma</button>
-                <div className="dropdown-content">
-                    <p>Lemma</p>
-                    <p>Identifier</p>
-                    <p>Interlingual Identifier</p>
-                </div>
-            </div>
-            <div className = "search-input">
-                <input placeholder="Please enter the word" type = "text" onChange={(event) => {setQuery(event.target.value)}}/>
-            </div>
-            <div className = "search-icon">
-                <img src = {magnifyingGlass} id = "magnifying-glass" alt = "magnifying glass"/>
-            </div>
+  return (
+    <div className="search-bar">
+      <div className="search-button">
+        <button className="drop-btn">Lemma</button>
+        <div className="dropdown-content">
+          <p>Lemma</p>
+          <p>Identifier</p>
+          <p>Interlingual Identifier</p>
         </div>
-    )
-}
+      </div>
+      <div className="search-input">
+        <input
+          placeholder="Please enter the word"
+          type="text"
+          onChange={(event) => {
+            setQuery(event.target.value);
+          }}
+          value={query}
+        />
+      </div>
+      <div className="search-icon">
+        <img
+          src={magnifyingGlass}
+          id="magnifying-glass"
+          alt="magnifying glass"
+          onClick={() => {
+            onSearch(query);
+            setQuery("");
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
