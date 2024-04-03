265447cda5e920a3b798d2e4f74ffa5a0f364328
uhditya <aditya.grg121@gmail.com>
2024-04-03 14:22:34 +0530

    misc

--- src/Components/Upload.jsx
+++ src/Components/Upload.jsx
@@ -8,19 +8,16 @@ function Upload() {
   };
 
   const handleUpload = () => {
-    // Here you can implement the file upload logic, such as sending the file to a server
-    // For demonstration purposes, we'll just log the file details
     if (selectedFile) {
       console.log('Selected file:', selectedFile);
-      // You can perform further operations here, like sending the file to a server using axios or Fetch API
     } else {
       console.log('No file selected');
     }
   };
 
   return (
-    <div style = {{alignContent : "center"}}>
-      <input type="file" onChange={handleFileChange} style = {{width : "21%", paddingBottom : "10px"}}/>
+    <div style = {{alignContent : "center", paddingLeft : "36%", paddingBottom : "15px"}}>
+      <input type="file" onChange={handleFileChange} style = {{width : "24%", paddingBottom : "10px"}}/>
       <button onClick={handleUpload}>Upload</button>
     </div>
   );
